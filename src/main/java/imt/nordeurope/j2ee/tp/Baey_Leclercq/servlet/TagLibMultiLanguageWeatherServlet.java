cff35a065c2ad0e61ccd9d27029c1b32c19bded9
VinLeclercq <qlqqlc@outlook.fr>
2022-01-02 21:28:37 +0100

    ptite modif

--- src/main/java/imt/nordeurope/j2ee/tp/Baey_Leclercq/servlet/TagLibMultiLanguageWeatherServlet.java
+++ src/main/java/imt/nordeurope/j2ee/tp/Baey_Leclercq/servlet/TagLibMultiLanguageWeatherServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "TagLibMultiLanguageWeather", value = "/TagLibMultiLanguageWeather")
+@WebServlet(name = "TagLibMultiLanguageWeatherServlet", value = "/TagLibMultiLanguageWeather")
 public class TagLibMultiLanguageWeatherServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
